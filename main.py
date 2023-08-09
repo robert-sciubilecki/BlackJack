961c27e631d9384c0008fcdbf8df945593e05b45
sciubilecki <sciubilecki.robert@gmail.com>
2023-08-09 14:05:55 +0200

    Just added some emoji for better user experience

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ clear()
 
 
 def populate_deck(faces):
-    colors = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+    colors = ['Spades ♠️', 'Hearts ♥️', 'Diamonds 🔶', 'Clubs ♣️']
     for face, value in faces:
         cards_dict[str(face)] = value
         for color in colors:
@@ -57,7 +57,8 @@ def calculate_hand_value(hand):
 def display_player_summary(player, hand):
     cards_on_hand = ', '.join(hand)
     cards_value = calculate_hand_value(hand)
-    print(f"{player} hand: {cards_on_hand}.\nTotal value of {player} cards: {cards_value}\n")
+    emoji = '🙂' if player == 'player' else '💻'
+    print(f"{emoji} {player} hand: {cards_on_hand}.\nTotal value of {player} cards: {cards_value}\n")
 
 
 computer_hand = []
@@ -74,7 +75,7 @@ def init():
     player_hand = deal_card(2)
 
     display_player_summary('Your', player_hand)
-    print('Computer has also 2 cards. You only see the first one.')
+    print('Computer has also 2 cards. You only see the first one.\n')
     display_player_summary('Computer', [computer_hand[0]])
     play_game()
 
@@ -91,11 +92,11 @@ def play_game():
             display_player_summary('Your', player_hand)
             current_hand_value = calculate_hand_value(player_hand)
             if current_hand_value == 21:
-                print('Black Jack, you win\n')
+                print('🏆 Black Jack, you win\n')
                 playing = False
                 replay()
             elif current_hand_value > 21:
-                print('Your hand is over 21 in total. You loose\n')
+                print('😔 Your hand is over 21 in total. You loose\n')
                 playing = False
                 replay()
             continue
@@ -103,7 +104,7 @@ def play_game():
             clear()
             playing = False
         else:
-            print("Wrong input, type 'y' or 'n'.\n")
+            print("🚫 Wrong input, type 'y' or 'n'.\n")
             continue
         final_computer_value = computer_round(computer_hand)
         check_winner(computer=final_computer_value, player=current_hand_value)
@@ -124,30 +125,30 @@ def computer_round(hand):
 
 def check_winner(computer, player):
 
-    display_player_summary('Player', player_hand)
+    display_player_summary('Your', player_hand)
     display_player_summary('Computer', computer_hand)
 
     if computer > player:
         if computer > 21:
-            print('Computer hand is over 21 in total. You win.\n')
+            print('🏆 Computer hand is over 21 in total. You win.\n')
         else:
-            print('computer wins\n')
+            print('😔 computer wins\n')
     elif player > computer:
-        print('player wins\n')
+        print('🏆 player wins\n')
     else:
-        print("It's a draw\n")
+        print("🟰 It's a draw\n")
 
 
 def replay():
-    play = input("Do you want to play again? Type 'y' or 'n': ").lower()
+    play = input("▶️ Do you want to play again? Type 'y' 👍 or 'n' 👎: ").lower()
     if play == 'y':
         clear()
         init()
     elif play == 'n':
         clear()
-        return print('Thank you for playing\n')
+        return print('👋 Thank you for playing\n')
     else:
-        print("Wrong input, type 'y' or 'n'.\n")
+        print("🚫 Wrong input, type 'y' or 'n'.\n")
         replay()
 
 
